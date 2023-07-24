928292eb1d367712126be18393d97b84bdbad479
rghvgpt <139127217+rghvgpt@users.noreply.github.com>
2023-07-24 11:44:26 +0530

    Update index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import profilePic from "public/ey_logo.png";
 export const Navbar = () => {
   return (
     <div className="flex h-[80px] sm:h-[80px] py-2 px-2 sm:px-8 items-center justify-center">
-      <Image src={profilePic} alt="EY Logo" width={150} height={50} />
+      <Image src={profilePic} alt="EY Logo" width={100} />
     </div>
   );
 };
