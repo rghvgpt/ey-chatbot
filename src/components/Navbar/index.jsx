377f56c260a6266ef18ecc742d58bd135212d99f
rghvgpt <139127217+rghvgpt@users.noreply.github.com>
2023-07-24 11:42:56 +0530

    Update index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import profilePic from "public/Logo-Vecmocon.svg";
+import profilePic from "public/ey_logo.png";
 
 export const Navbar = () => {
   return (
     <div className="flex h-[80px] sm:h-[80px] py-2 px-2 sm:px-8 items-center justify-center">
-      <Image src={profilePic} alt="Vecmocon Logo" width={150} height={50} />
+      <Image src={profilePic} alt="EY Logo" width={150} height={50} />
     </div>
   );
 };
